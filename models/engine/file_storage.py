29d8903fde08aafc8761db998f9322005309553d
fabianatellechea <fabianatellechea@gmail.com>
2023-07-07 19:03:00 -0700

    rm tests & pycodestyle fixed

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -17,14 +17,14 @@ class FileStorage:
         # Obtenemos el nombre de la calse "obj" y luego el id
         key = f"{obj.__class__.__name__}.{obj.id}"
         self.__objects[key] = obj
-    
+
     def save(self):
         obj_dict = {}
         for key, value in self.__objects.items():
             obj_dict[key] = value.to_dict()
 
         with open(self.__file_path, 'w') as file:
-             json.dump(obj_dict, file)
+            json.dump(obj_dict, file)
 
     def reload(self):
         if os.path.exists(self.__file_path):
