e508076d9d79d915ac796dba58332a01a6ae816d
fabianatellechea <fabianatellechea@gmail.com>
2023-07-09 19:08:42 -0700

    test base model

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,14 +1,49 @@
 #!/usr/bin/python3
 from models.base_model import BaseModel
+import unittest
+from datetime import datetime
 
-my_model = BaseModel()
-my_model.name = "My First Model"
-my_model.my_number = 89
-print(my_model)
-my_model.save()
-print(my_model)
-my_model_json = my_model.to_dict()
-print(my_model_json)
-print("JSON of my_model:")
-for key in my_model_json.keys():
-    print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
+
+class test_BaseModel(unittest.TestCase):
+    """test BaseModel class"""
+
+    def test_creation(self):
+        """checks if an instance is created"""
+        bm = BaseModel()
+        self.assertIsInstance(bm, BaseModel)
+
+    def test_id_instance(self):
+        """checks instance id"""
+        bm = BaseModel()
+        self.assertIsInstance(bm.id, str)
+
+    def test_updated_at(self):
+        """checks updated_at"""
+        bm = BaseModel()
+        self.assertIsInstance(bm.updated_at, datetime)
+
+    def test_save(self):
+        """checks save method"""
+        bm = BaseModel()
+        o_datetime = bm.updated_at
+        bm.save()
+        n_datetime = bm.updated_at
+        self.assertNotEqual(o_datetime, n_datetime)
+
+    def test_to_dict(self):
+        """checks to_dict method"""
+        bm = BaseModel()
+        new_dict = bm.to_dict()
+        self.assertIsInstance(new_dict, dict)
+        self.assertIsInstance(new_dict["id"], str)
+        self.assertIsInstance(new_dict["created_at"], str)
+        self.assertIsInstance(new_dict["updated_at"], str)
+    
+    def test_str(self):
+        """checks str method"""
+        bm = BaseModel()
+        string = f"[{bm.__class__.__name__}] ({bm.id}) {bm.__dict__}"
+        self.assertEqual(str(bm), string)
+
+    if __name__ == "__main__":
+            unittest.main()
